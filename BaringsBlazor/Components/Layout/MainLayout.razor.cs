1ae1a2868b51e4c0f878566cb4b7735ece048b6a
Glen Wilkin <glen@luthansolutions.com>
2025-02-24 10:22:56 +0000

    Add blazor app

---
+++ BaringsBlazor/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,24 @@
+﻿namespace BaringsBlazor.Components.Layout;
+
+using MudBlazor;
+
+public sealed partial class MainLayout
+{
+    bool _drawerOpen = false;
+
+    void DrawerToggle()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+
+    private readonly MudTheme theme = new()
+    {
+        PaletteLight = new()
+        {
+            Primary = Colors.Blue.Darken3,
+            Secondary = Colors.Red.Darken2,
+            AppbarBackground = Colors.Blue.Darken1
+        },
+        ZIndex = new() { Popover = new ZIndex().Dialog + 1 }
+    };
+}
