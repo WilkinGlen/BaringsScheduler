240f2af22501335ad3a916d12b6a8b6d7e6d3c81
Glen Wilkin <glen@luthansolutions.com>
2025-03-02 14:13:34 +0000

    Add dark mode toggling

--- BaringsQuartzUI/Components/Layout/MainLayout.razor.cs
+++ BaringsQuartzUI/Components/Layout/MainLayout.razor.cs
@@ -1,9 +1,24 @@
 ﻿namespace BaringsQuartzUI.Components.Layout;
 
+using MudBlazor;
+using System.Threading.Tasks;
+
 public sealed partial class MainLayout
 {
+    private MudThemeProvider? themeProvider;
+
     private bool drawerOpen;
     private string? pageTitle = " - Home";
+    private bool isDarkMode = false;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            this.isDarkMode = await this.themeProvider!.GetSystemPreference();
+            this.StateHasChanged();
+        }
+    }
 
     private void DrawerToggle(string? pageTitle)
     {
@@ -14,4 +29,6 @@ public sealed partial class MainLayout
 
         this.drawerOpen = !this.drawerOpen;
     }
+
+    private void DarlModeToggle() => this.isDarkMode = !this.isDarkMode;
 }
